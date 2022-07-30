8253c58096e29bfec246807f52f1e551233cf040
SLingkon <you@example.com>
2022-07-30 21:28:31 +0600

    new commit

---
+++ myscript.js
@@ -0,0 +1,23 @@
+//fetch method
+
+fetch("https://fakestoreapi.com/products")
+          .then((data) => {
+                    return data.json();
+          })
+          .then((dataComplete) => {
+                    let data1 = "";
+                    dataComplete.map((values) => {
+                              data1 += `<div class="card" style="width:300px">
+                              <div class="card-body ">
+                              <h4 class=" title card-title text-center" id="heading">${values.title}</h4>
+                              <img class="card-img flex-wrap" src=${values.image} alt="Card image" class="images">
+                                  <p class="card-text">${values.description}</p>
+                                  <p class="card-text text-center text-uppercase font-weight-bold category">${values.category}</p>
+                                  <p class="card-text price">${values.price}</p>
+                              </div> `;
+                    });
+                    document.getElementById("cards").innerHTML = data1;
+          })
+          .catch((err) => {
+                    console.log(err);
+          });
